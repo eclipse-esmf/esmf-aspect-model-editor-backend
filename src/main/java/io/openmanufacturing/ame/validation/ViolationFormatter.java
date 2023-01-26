b8458b1abe1ebcd08ccab4656f47fcc433247c73
Michele Santoro <michele.santoro@de.bosch.com>
2023-01-26 09:47:29 +0100

    Add validation visitor methods for all possibilities

--- src/main/java/io/openmanufacturing/ame/validation/ViolationFormatter.java
+++ src/main/java/io/openmanufacturing/ame/validation/ViolationFormatter.java
@@ -23,8 +23,31 @@ import io.openmanufacturing.ame.exceptions.UrnNotFoundException;
 import io.openmanufacturing.ame.model.validation.ViolationError;
 import io.openmanufacturing.ame.services.utils.ModelUtils;
 import io.openmanufacturing.sds.aspectmodel.shacl.fix.Fix;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.ClassTypeViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.ClosedViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.DatatypeViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.DisjointViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.EqualsViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.InvalidValueViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.JsConstraintViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.LanguageFromListViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.LessThanOrEqualsViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.LessThanViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.MaxCountViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.MaxExclusiveViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.MaxInclusiveViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.MaxLengthViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.MinCountViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.MinExclusiveViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.MinInclusiveViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.MinLengthViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.NodeKindViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.NotViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.PatternViolation;
 import io.openmanufacturing.sds.aspectmodel.shacl.violation.ProcessingViolation;
 import io.openmanufacturing.sds.aspectmodel.shacl.violation.SparqlConstraintViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.UniqueLanguageViolation;
+import io.openmanufacturing.sds.aspectmodel.shacl.violation.ValueFromListViolation;
 import io.openmanufacturing.sds.aspectmodel.shacl.violation.Violation;
 import io.openmanufacturing.sds.aspectmodel.urn.AspectModelUrn;
 
@@ -38,7 +61,7 @@ public class ViolationFormatter
 
       final List<ViolationError> violationErrors = new ArrayList<>();
       final List<Violation> nonSemanticViolations = filterNonSemanticViolations( violations );
-      
+
       if ( !nonSemanticViolations.isEmpty() ) {
          return processNonSemanticViolation( nonSemanticViolations, violationErrors );
       }
@@ -101,6 +124,167 @@ public class ViolationFormatter
       final ViolationError violationError = visit( violation );
       violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
 
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitMinCountViolation( final MinCountViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitMaxCountViolation( final MaxCountViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitClassTypeViolation( final ClassTypeViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitDatatypeViolation( final DatatypeViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitInvalidValueViolation( final InvalidValueViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitLanguageFromListViolation( final LanguageFromListViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitMaxExclusiveViolation( final MaxExclusiveViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitMaxInclusiveViolation( final MaxInclusiveViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitMaxLengthViolation( final MaxLengthViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitMinExclusiveViolation( final MinExclusiveViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitMinInclusiveViolation( final MinInclusiveViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitMinLengthViolation( final MinLengthViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitNodeKindViolation( final NodeKindViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitPatternViolation( final PatternViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitJsViolation( final JsConstraintViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitUniqueLanguageViolation( final UniqueLanguageViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitEqualsViolation( final EqualsViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitDisjointViolation( final DisjointViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitLessThanViolation( final LessThanViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitLessThanOrEqualsViolation( final LessThanOrEqualsViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitValueFromListViolation( final ValueFromListViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitClosedViolation( final ClosedViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
+      return violationError;
+   }
+
+   @Override
+   public ViolationError visitNotViolation( final NotViolation violation ) {
+      final ViolationError violationError = visit( violation );
+      violationError.setFocusNode( AspectModelUrn.fromUrn( violation.context().element().getURI() ) );
       return violationError;
    }
 
