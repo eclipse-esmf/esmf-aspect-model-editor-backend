116d8a35d9ad45fe7d7f17b618c4140af1ccb98b
Michele Santoro <michele.santoro@de.bosch.com>
2023-11-02 15:54:13 +0100

    Revert validation import

--- src/main/java/org/eclipse/esmf/ame/exceptions/model/ErrorResponse.java
+++ src/main/java/org/eclipse/esmf/ame/exceptions/model/ErrorResponse.java
@@ -13,12 +13,13 @@
 
 package org.eclipse.esmf.ame.exceptions.model;
 
+import javax.validation.constraints.NotEmpty;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
