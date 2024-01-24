6feb640a86d85b68b67d3bbb96bc4579a56ed880
Michele Santoro <michele.santoro@de.bosch.com>
2024-01-24 11:36:34 +0100

    Fix doc problem

--- aspect-model-editor-runtime/src/main/java/org/eclipse/esmf/ame/Application.java
+++ aspect-model-editor-runtime/src/main/java/org/eclipse/esmf/ame/Application.java
@@ -19,6 +19,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
    public static void main( final String[] args ) {
+      NativeImageHelpers.ensureRequiredEnvironment();
       SpringApplication.run( Application.class, args );
    }
 }
