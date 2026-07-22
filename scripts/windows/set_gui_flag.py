ad90a50daee711f848dce7f4cb8c6decb67f1ea6
Michele Santoro <michele.santoro@de.bosch.com>
2026-07-22 12:36:23 +0200

    Update set_gui_flag.py

--- scripts/windows/set_gui_flag.py
+++ scripts/windows/set_gui_flag.py
@@ -1,31 +1,18 @@
 #!/usr/bin/env python3
 
-import pefile
-import shutil
 import sys
 
-from tempfile import NamedTemporaryFile
 
+def set_gui_subsystem(path):
+    with open(path, 'r+b') as f:
+        f.seek(0x3C)
+        pe_offset = int.from_bytes(f.read(4), 'little')
 
-def set_subsystem_type(path, subsystem_type):
-    with open(path, 'rb') as f:
-        original_data = f.read()
+        subsystem_offset = pe_offset + 0x5C
+        f.seek(subsystem_offset)
 
-    with pefile.PE(path, fast_load=True) as pe:
-        pe_end = max(s.PointerToRawData + s.SizeOfRawData for s in pe.sections)
-        pe.OPTIONAL_HEADER.Subsystem = dict(pefile.subsystem_types)[subsystem_type]
-        temp = NamedTemporaryFile(delete=False)
-        pe.write(temp.name)
-        temp.close()
-
-    # Append overlay data (warp tarball)
-    overlay = original_data[pe_end:]
-    if overlay:
-        with open(temp.name, 'ab') as f:
-            f.write(overlay)
-
-    shutil.move(temp.name, path)
+        f.write((2).to_bytes(2, 'little'))
 
 
 if __name__ == "__main__":
-    set_subsystem_type(sys.argv[1], "IMAGE_SUBSYSTEM_WINDOWS_GUI")
+    set_gui_subsystem(sys.argv[1])
